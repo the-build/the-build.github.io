4b5a9b90dc7c1e89eca519ca86ab953b2ff02365
hahn079 <hahn079@gmail.com>
2025-04-28 00:11:30 +0900

    [feature] add google ad-sense

---
+++ gatsby-ssr.js
@@ -0,0 +1,15 @@
+// gatsby-ssr.js
+import React from "react";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    // <script
+    //   key="adsense-script"
+    //   data-ad-client="ca-pub-9813975852186273"  // 여기에 본인 코드 넣기
+    //   async
+    //   src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    // ></script>,
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9813975852186273"
+     crossorigin="anonymous"></script>
+  ]);
+};
